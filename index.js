f0274047657cf222e25b9daab39bb6b978413781
Tah56 <tanzimahmed575@gmail.com>
2026-05-25 20:22:55 +0600

    hide url

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const client = new MongoClient(uri, {
   },
 });
 const JWKS =  createRemoteJWKSet(
-  new URL(("http://localhost:3000/api/auth/jwks"))
+  new URL((`${process.env.CLIENT_URI}/api/auth/jwks`))
 )
 
 const verifyToken = async (req, res, next) => {
